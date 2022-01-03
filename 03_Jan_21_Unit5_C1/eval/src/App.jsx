d947b21e43034926a3edea00dc416738e634f261
Shivam Sharma <mechbuddy.shivam@gmail.com>
2022-01-03 18:02:28 +0530

    eval

--- 03_Jan_21_Unit5_C1/eval/src/App.jsx
+++ 03_Jan_21_Unit5_C1/eval/src/App.jsx
@@ -4,9 +4,15 @@ import {Input, Div,Div1} from './components/styled_components'
 
 function App() {
 
-  const [title,setTitle] = useState("");
-  const [ingredient,setIngredient] = useState("")
-  const [recipe, setRecipe] = useState([]);
+  const [recipe,setRecipe] = useState([]);
+  const [form , setForm] = useState({     
+    title:"",
+    ingredients:"",
+    time:"",
+    image:"",
+    instructions:"",
+});
+
 
  useEffect(() => {
     getRecipe();
@@ -21,12 +27,22 @@ function App() {
         setRecipe(res);
       });
   };
+
+  const handleChange=(e)=>{
+        
+    const {name, value} = e.target;
+
+    setForm({
+        ...form,
+        [name]: value,
+    });
+ }
+
   
   const addRecipe = (e) => {
-  const payload = {
-    title:title,
-     
-  };
+    console.log(form)
+    e.preventDefault();
+  const payload = form;
   fetch("http://localhost:3002/recipe", {
     method: "POST",
     body: JSON.stringify(payload),
@@ -35,40 +51,48 @@ function App() {
     },
   }).then(()=>{
      getRecipe()
-      setTitle("");
+      setForm("");
   })
 };
 
-  return (
+
+
+const detail=(id) => {
+  fetch(`http://localhost:3002/recipe/${id}`)
+  .then((d) => d.json())
+  .then((res)=>{
    
+  })
+}
+
+  return (
+
     <div className="App">
       <h1>Cook@Home</h1>
       <div className="container">
       <Div>
       <h2>Add Recipe</h2>
-    <Input id="title" value={title} type="text"
-        onChange={(e) => {
-          setTitle(e.target.value);
-        }} placeholder="Title"/>
-    <Input id="ingredients" value={ingredient} onChange={(e) => {
-          setIngredient(e.target.value);
-        }}  placeholder=" Ingredients" required/>
-    <Input id="time" placeholder="Time to cook" required/> 
-    <Input type="file" placeholder="Image" required/>
-    <Input id="instructions" placeholder="Instructions" required/>
-    <Input id="submit" type="submit" value="Submit" onSubmit={addRecipe} required/>
+      <form onSubmit={addRecipe}>
+    <Input name="title"  type="text" onChange={handleChange} placeholder="Title"/>
+    <Input name="ingredients"  onChange={handleChange} placeholder=" Ingredients" required/>
+    <Input name="time"  onChange={handleChange} placeholder="Time to cook" required/> 
+    <Input name="image" type="file"  onChange={handleChange} placeholder="Image" />
+    <Input name="instructions"  onChange={handleChange} placeholder="Instructions" required/>
+    <Input id="submit"  onChange={handleChange} type="submit" value="Submit" required/>
+    </form>
     </Div>
 
    <Div1 id="collection">
 <h2>Recipes</h2>
 {recipe.map((e) => (
-        <li>{e.title}</li>
+        <li >{e.title}</li>
       ))}
    </Div1>
       </div>
    
    <div id="recipe">
      <h2>Description</h2>
+
    </div>
     </div>
   );
