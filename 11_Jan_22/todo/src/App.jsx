ac5666e10c47977197c4030cff443d181f6a05a3
Shivam Sharma <mechbuddy.shivam@gmail.com>
2022-01-15 03:37:51 +0530

    Redux todo using hooks

---
+++ 11_Jan_22/todo/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import {Todo} from './components/Todo'
+import {EditTodo} from './components/EditTodo'
+import { Route, Routes ,Link} from "react-router-dom";
+import { Details } from './components/Details';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <header>
+        <Link to={"/"}>
+        <img src='https://learn.masaischool.com/img/logo-navbar.svg'/>  
+        </Link>
+        todos
+  
+      </header>
+      <Routes>
+      <Route path="/" element={<Todo/>}></Route> 
+      <Route path="/todo/:id/edit" element={<EditTodo/>}></Route>
+      <Route path="/todo/:id" element={<Details/>}></Route>
+      </Routes>
+    </div>
+  );
+}
+export default App;
