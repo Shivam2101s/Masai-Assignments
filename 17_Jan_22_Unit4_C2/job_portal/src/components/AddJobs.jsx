8987006f8a9d8f93edec78909d27f128fc4a01c6
Shivam Sharma <mechbuddy.shivam@gmail.com>
2022-01-17 16:30:26 +0530

    1

--- 17_Jan_22_Unit4_C2/job_portal/src/components/AddJobs.jsx
+++ 17_Jan_22_Unit4_C2/job_portal/src/components/AddJobs.jsx
@@ -1,21 +1,32 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import {addJob} from "../features/actions"
 
 
 export const AddJobs = () => {
+const [form,setForm] = useState([]);    
+
+const handleChange = (e) => {
+    const {name,value} = e.target;
+    setForm({
+        ...form,
+        [name]: value,
+    })
+ }
 
 const dispatch = useDispatch();
 
     const addJobs = () => { 
-          fetch("http://localhost:3002/todos", {
+          fetch("http://localhost:3003/jobs", {
             method: "POST",
-            body: JSON.stringify(),
+            body: JSON.stringify(form),
             headers: { "Content-Type": "application/json" },
           })
             .then((d) => d.json())
             .then((res) => {
-             dispatch(addJob)
-              alert("New job added successfully");
+                alert("New job added successfully");
+             dispatch(addJob);
+              
             })
             .catch((err) => {
               console.log(err);
@@ -23,15 +34,15 @@ const dispatch = useDispatch();
       };
 
     return (
-        <div id="jobDiv"> 
+        <div class="jobDiv"> 
         <form onSubmit={addJobs}>
-            <input type="text" name="title" placeholder="Job title"  required/>
-            <input type="text" name="company"   placeholder="Company" required/>
-            <input type="number" name="salary" placeholder="Salary" required/>
-            <input type="text" name="description" placeholder="Job description" required/>
-            <input type="text" name="location" placeholder="Location" required/>
-            <input type="text" name="type" placeholder="Job type" required/>
-            <input type="submit"  name="submit" value="Add Job"/>
+            <input type="text" onChange={handleChange} name="title" placeholder="Job title"  required/>
+            <input type="text" onChange={handleChange} name="company"   placeholder="Company" required/>
+            <input type="number" onChange={handleChange} name="salary" placeholder="Salary" required/>
+            <input type="text" onChange={handleChange} name="description" placeholder="Job description" required/>
+            <input type="text" onChange={handleChange} name="location" placeholder="Location" required/>
+            <input type="text" onChange={handleChange} name="type" placeholder="Job type" required/>
+            <input type="submit" onChange={handleChange} id="addJobBtn" name="submit" value="Add Job"/>
         </form>
 
         </div>
